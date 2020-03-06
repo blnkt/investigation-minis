5f652ec70aa5c837742cd93c5488c6b4db25b22b
Blake Mason <bmason@lsst.org>
2020-03-06 12:16:37 -0700

    [C] Update ui chrome with branding and simplify
    Header has no table of contents, Vera Rubin Observatory logo, background
    color, and cnetered/width restricted layout;

--- src/components/site/header/index.jsx
+++ src/components/site/header/index.jsx
@@ -1,58 +1,34 @@
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Toolbar } from 'react-md';
-import Button from '../button/index';
-import logo from '../../../images/lsst-logo.svg';
-import ButtonIcon from '../button/ButtonIcon';
-import Close from '../icons/Close';
-import Menu from '../icons/Menu';
+import logo from '../../../images/vro-logo.png';
 
-import styles from './header.module.scss';
+import { headerInner, logoWrapper, siteLogo } from './header.module.scss';
 
 class Header extends React.PureComponent {
   render() {
-    const { siteTitle, toggleSidebar, sidebarVisiblity } = this.props;
+    const { siteTitle } = this.props;
 
     return (
       <>
         <Toolbar
           colored
           fixed
-          title="LSST"
+          title="Vera Rubin Observatory"
           titleClassName="screen-reader-only"
           className="header-primary"
-          nav={
-            <Button
-              icon
-              iconEl={
-                sidebarVisiblity ? (
-                  <ButtonIcon srText="Close Table of Contents" Icon={Close} />
-                ) : (
-                  <ButtonIcon srText="Open Table of Contents" Icon={Menu} />
-                )
-              }
-              onClick={toggleSidebar}
-            />
-          }
         >
-          <div className={styles.headerInner}>
-            <Link to="/" className={styles.logoWrapper}>
+          <div className={headerInner}>
+            <span className={logoWrapper}>
               <span className="screen-reader-only">Home</span>
               <img
                 aria-hidden
                 src={logo}
                 alt={siteTitle}
-                className={styles.siteLogo}
+                className={siteLogo}
               />
-            </Link>
+            </span>
           </div>
-          {/* <LinearProgress className="{styles.pageProgress} value={pageProgress} /> */}
-          {/*        <SiteNav
-            menuOpen={menuOpen}
-            handleClose={this.closeMenu}
-            handleClick={this.clickHandler}
-          /> */}
         </Toolbar>
       </>
     );
@@ -61,8 +37,6 @@ class Header extends React.PureComponent {
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-  sidebarVisiblity: PropTypes.bool,
-  toggleSidebar: PropTypes.func,
 };
 
 Header.defaultProps = {
